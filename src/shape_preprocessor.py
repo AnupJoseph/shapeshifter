5c9e997438a1fce158b58349fe92e999b88f4ff1
Anup Joseph <anup20joseph@gmail.com>
2023-11-19 19:27:01 +0530

    Added path preprocessor

---
+++ src/shape_preprocessor.py
@@ -0,0 +1,19 @@
+# Internal
+from xml.dom import minidom
+import re
+
+# External
+
+# Project
+
+# Project Specific Configuration
+
+
+def grab_points(infile: str):
+    doc = minidom.parse(infile)
+    path_strings = [path.getAttribute("d") for path in doc.getElementsByTagName("path")]
+    pattern = r"\bQ .*? L\b"
+    matches = re.findall(pattern, path_strings[0])
+    svgpath = matches[0].replace("Q ", "").replace(" L", "")
+    svgpath = [[float(j) for j in i.split(",")] for i in svgpath.split()]
+    return svgpath
